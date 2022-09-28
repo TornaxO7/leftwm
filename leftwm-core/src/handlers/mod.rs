c07af9775cfa6810f87fe1bde522b0689039142b
TornaxO7 <tornax07@gmail.com>
2022-09-29 01:02:53 +0200

    moving all window handlers to the directory

--- leftwm-core/src/handlers/mod.rs
+++ leftwm-core/src/handlers/mod.rs
@@ -5,12 +5,10 @@ mod goto_tag_handler;
 mod mouse_combo_handler;
 mod screen_create_handler;
 mod window_handler;
-mod window_move_handler;
-mod window_resize_handler;
 
 use super::command::Command;
 use super::config::Config;
 use super::models::{
-    Manager, Mode, Screen, Window, WindowChange, WindowHandle, WindowType, Workspace,
+    Manager, Mode, Screen, Window, WindowHandle, WindowType, Workspace,
 };
 use super::DisplayEvent;
